053530b8d575218afcd29d21e0a4f344246959a6
lexardi <lexardi.lab@gmail.com>
2024-04-17 20:54:55 +0200

    quit componnet

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,9 +1,15 @@
-import Hero from "./components/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center ">
-      <Hero />
+      <Image
+        width="1928"
+        height="600"
+        src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1675546550/gaizka.me/index/itx_1_v2dzog.webp"
+        alt="Itzina"
+        className="w-full h-screen"
+      />{" "}
     </div>
   );
 }
