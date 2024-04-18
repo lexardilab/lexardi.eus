75fc3d687b24f74700e55f91076fc0f0a794c9e9
lexardi <lexardi.lab@gmail.com>
2024-04-18 08:35:13 +0200

    journal

--- src/app/journal/page.jsx
+++ src/app/journal/page.jsx
@@ -1,28 +1,3 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import Card from "../components/Card";
-import { getAllJournals } from "../../../sanity/lib/journal-util";
 export default function page() {
-  useEffect(() => {
-    useEffect(() => {
-      const fetchData = async () => {
-        const journals = await getAllJournals();
-        setData(journals);
-      };
-      fetchData();
-    }, []);
-    const fetchData = async () => {
-      const journals = await getAllJournals();
-      setData(journals);
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <div className="grid h-screen grid-cols-4 bg-slate-100">
-      {Journals?.map((journal) => (
-        <Card key={journal._id} product={journal} />
-      ))}
-    </div>
-  );
+  return <div className="grid h-screen grid-cols-4 bg-slate-100">journal</div>;
 }
