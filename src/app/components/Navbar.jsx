cb75c41628040bfa2248c8e1ddc05b1f4d19fc18
lexardi <lexardi.lab@gmail.com>
2024-04-24 09:25:48 +0200

    navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,6 +4,8 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { montserrat, roboto_mono } from "../utils/fonts";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -17,7 +19,16 @@ export default function BasicMenu() {
 
   return (
     <div className="flex items-center justify-between px-6 py-4">
-      <div>Logo</div>
+      <div>
+        <Link href="/">
+          <Image
+            src="/borobil_yellow.svg"
+            width={40}
+            height={100}
+            alt="Lexardi Logo"
+          />
+        </Link>
+      </div>
       <div className="flex ">
         <h1 className={`${roboto_mono.className} text-sm text-black px-2`}>
           Tienda
