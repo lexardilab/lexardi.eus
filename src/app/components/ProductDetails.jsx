a22287000fd757f5f9c066c5516991723b5c76ec
lexardi <lexardi.lab@gmail.com>
2024-04-25 08:07:50 +0200

    product details

--- src/app/components/ProductDetails.jsx
+++ src/app/components/ProductDetails.jsx
@@ -8,11 +8,6 @@ export default function ProductDetails({ product }) {
       <div className="flex items-center justify-center pt-20">
         <h1 className={`${roboto_mono.className} text-4xl`}>{product?.name}</h1>
       </div>
-      <div className="flex justify-center pt-12">
-        <h1 className={`${montserrat.className} text-xl font-semibold`}>
-          {product?.subtitle}
-        </h1>
-      </div>
       <div className="flex justify-center">
         <div className="pt-12 w-[600px]">
           <h1 className={`${montserrat.className} text-sm text-center `}>
@@ -49,17 +44,7 @@ export default function ProductDetails({ product }) {
           />
         </div>
       </div>
-      <div className="flex justify-center py-12">
-        {product?.extraImages?.map((image) => (
-          <Image
-            src={image}
-            width="500"
-            height="600"
-            alt="Gallery"
-            className="px-4"
-          />
-        ))}
-      </div>
+
       <div className="grid grid-cols-2 gap-4 px-4 pb-6">
         <div className="px-4">
           <h1 className={`${montserrat.className} text-sm`}>
