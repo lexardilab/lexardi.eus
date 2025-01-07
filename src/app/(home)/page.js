4a198c5bca3dd8c0c86f9b917b62e8cbff26a498
lexardi <lexardi.lab@gmail.com>
2025-01-07 16:56:13 +0100

    pruebas

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -3,16 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex items-center justify-center ">
-      <Image
-        width="1928"
-        height="600"
-        src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1675546550/gaizka.me/index/itx_1_v2dzog.webp"
-        alt="Itzina"
-        className="w-full h-screen"
-      />
-    </div>
-  
+      <div className="flex items-center justify-center ">
+        <Image
+          width="1928"
+          height="600"
+          src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1675546550/gaizka.me/index/itx_1_v2dzog.webp"
+          alt="Itzina"
+          className="w-full h-screen"
+        />
+      </div>
+
     </>
   );
 }
