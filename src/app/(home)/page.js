5a7956ac7189cb00d4676c7420a6cfdfe2ec5d3d
lexardi <lexardi.lab@gmail.com>
2025-01-07 15:57:44 +0100

    favicon

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <div className="flex items-center justify-center ">
       <Image
         width="1928"
@@ -9,7 +10,9 @@ export default function Home() {
         src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1675546550/gaizka.me/index/itx_1_v2dzog.webp"
         alt="Itzina"
         className="w-full h-screen"
-      />{" "}
+      />
     </div>
+  
+    </>
   );
 }
