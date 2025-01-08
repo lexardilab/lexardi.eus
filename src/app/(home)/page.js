85ca62e32e500b088dcd10a4b8faab2b31c4a478
lexardi <lexardi.lab@gmail.com>
2025-01-08 09:42:59 +0100

    commit

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -12,6 +12,21 @@ export default function Home() {
           className="w-full h-screen"
         />
       </div>
+      <div className="pt-12 flex justify-center pb-4">
+        <Image
+          width="600"
+          height="50"
+          src="/Logo_Harri.jpg"
+          alt="Itzina"
+          className=""
+        />
+
+      </div>
+      <div className="flex justify-center pt-6 pb-12">
+        <h1 className="text-center">Mollit et proident voluptate labore commodo cillum. Dolore mollit reprehenderit aliqua labore in aliquip fugiat mollit.<br />
+          Eu ipsum elit velit enim tempor minim labore voluptate incididunt anim voluptate fugiat. Lorem mollit do dolor aute ad.
+        </h1>
+      </div >
 
     </>
   );
