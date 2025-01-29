1b9500332bc7962fb4950676d17124bc1de9d0a4
lexardi <lexardi.lab@gmail.com>
2025-01-29 14:45:05 +0100

    home

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -20,24 +20,12 @@ export default function Home() {
           alt="Itzina"
           className=""
         />
-
       </div>
       <div className="flex justify-center pt-4 pb-12">
-        <h1 className="text-center font-thin text-5xl">azpitik dena sutan dabil
+        <h1 className="text-center font-thin text-5xl">
+          azpitik dena sutan dabil
         </h1>
-      </div >
-<<<<<<< HEAD
-      <div className="grid grid-cols-4 gap-4 px-4 pb-12">
-        <div className="bg-red-200">01</div>
-        <div className="bg-blue-200">02</div>
-        <div className="bg-orange-200">03</div>
-        <div className="bg-yellow-200">04</div>
-
       </div>
-
-
-=======
->>>>>>> 307fe7fc36eff6d05f598c98a5488b593859a44e
     </>
   );
 }
