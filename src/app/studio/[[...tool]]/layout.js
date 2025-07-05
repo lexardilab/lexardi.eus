eff9a6cec550cc2c2ce72c2f8681771d53ae88a8
lexardi <lexardi.lab@gmail.com>
2025-07-05 12:16:28 +0200

    sanity

---
+++ src/app/studio/[[...tool]]/layout.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: "Lexardi Lab | Studio",
+  description: " ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
