8756e2c02cff919c35d04222a48feb88e66b81e8
lexardi <lexardi.lab@gmail.com>
2025-07-08 21:17:28 +0200

    sendgrid

---
+++ src/app/contacto/page.jsx
@@ -0,0 +1,11 @@
+// pages/contacto.js
+import ContactForm from '@/components/ContactForm';
+
+export default function Contacto() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Contáctame</h1>
+      <ContactForm />
+    </div>
+  );
+}
