e626ca31bfbbe86cb054e2fbab73fcc7a4d36bd5
Lexardi <lexardi.lab@gmail.com>
2025-10-30 09:21:35 +0100

    Gallery

--- src/components/HorizontalGallery.jsx
+++ src/components/HorizontalGallery.jsx
@@ -94,7 +94,7 @@ export default function HorizontalGallery({ images }) {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="20ncu0"
+            width="200"
             height="200"
             viewBox="0 0 24 24"
             fill="none"
